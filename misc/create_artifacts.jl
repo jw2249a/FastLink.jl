8fbdd1b4c4e4fac0ff25307bc38161ff5a539004
jw2249a <github@jackryanwilliams.com>
2024-01-09 01:53:17 +0000

    targz test data

---
+++ misc/create_artifacts.jl
@@ -0,0 +1,11 @@
+using SHA, Inflate, Tar
+fileA = "test/dfA.tar.gz"
+fileB = "test/dfB.tar.gz"
+
+println("sha256 = ", bytes2hex(open(sha256, fileA)))
+println("git-tree-sha1 = ", Tar.tree_hash(IOBuffer(inflate_gzip(fileA))))
+
+println("sha256 = ", bytes2hex(open(sha256, fileB)))
+println("git-tree-sha1 = ", Tar.tree_hash(IOBuffer(inflate_gzip(fileB))))
+
+
